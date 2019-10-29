4ef5788f1a32ea8a9f489e1e2db01db429b79ef7
Dylan Lu <dyl055a@gmail.com>
2019-10-28 21:29:09 -0700

    Update code for new energies and angles and remove zero hit events

--- preprocessing/CNN_endcaps_npz_to_h5.py
+++ preprocessing/CNN_endcaps_npz_to_h5.py
@@ -19,11 +19,21 @@ def parse_args():
     return args
 
 def count_events(files):
+    # Because we want to remove events with 0 hits, 
+    # we need to count the events beforehand (to create the h5 file).
+    # This function counts and indexes the events with more than 0 hits.
+    # Files need to be iterated in the same order to use the indexes.
     num_events = 0
-    for f in files:
-        data = np.load(f)
-        num_events += data['event_id'].shape[0]
-    return num_events
+    nonzero_file_events = []
+    for file_index, f in enumerate(files):
+        data = np.load(f, allow_pickle=True)
+        nonzero_file_events.append([])
+        hits = data['digi_hit_pmt']
+        for i in range(len(hits)):
+            if len(hits[i]) != 0:
+                nonzero_file_events[file_index].append(i)
+                num_events += 1
+    return (num_events, nonzero_file_events)
 
 def GenMapping(csv_file):
 
@@ -46,26 +56,21 @@ if __name__ == '__main__':
 # -- Parse arguments
     config = parse_args()
 
-    #read in the input file list
+    # Read in the input file list
     with open(config.input_file_list[0]) as f:
         files = f.readlines()
 
-    #remove whitespace 
+    # Remove whitespace 
     files = [x.strip() for x in files] 
      
-    # -- Check that files were provided
+    # Check that files were provided
     if len(files) == 0:
         raise ValueError("No files provided!!")
     print("Merging "+str(len(files))+" files")
     
-    #print("Files are:")
-    #print(files)
-    
-    
-  # -- Start merging
-    num_events = count_events(files)
-    
-    # Only use 32 bit precision
+    # Start merging
+    num_nonzero_events, nonzero_event_indexes = count_events(files)
+    print(num_nonzero_events) 
     dtype_events = np.dtype(np.float32)
     dtype_labels = np.dtype(np.int32)
     dtype_energies = np.dtype(np.float32)
@@ -75,25 +80,25 @@ if __name__ == '__main__':
     dtype_angles = np.dtype(np.float32)
     h5_file = h5py.File(config.output_file[0], 'w')
     dset_event_data = h5_file.create_dataset("event_data",
-                                       shape=(num_events,)+IMAGE_SHAPE,
+                                       shape=(num_nonzero_events,)+IMAGE_SHAPE,
                                        dtype=dtype_events)
     dset_labels = h5_file.create_dataset("labels",
-                                   shape=(num_events,),
+                                   shape=(num_nonzero_events,),
                                    dtype=dtype_labels)
     dset_energies = h5_file.create_dataset("energies",
-                                     shape=(num_events, 1),
+                                     shape=(num_nonzero_events, 1),
                                      dtype=dtype_energies)
     dset_positions = h5_file.create_dataset("positions",
-                                      shape=(num_events, 1, 3),
+                                      shape=(num_nonzero_events, 1, 3),
                                       dtype=dtype_positions)
     dset_IDX = h5_file.create_dataset("event_ids",
-                                shape=(num_events,),
+                                shape=(num_nonzero_events,),
                                 dtype=dtype_IDX)
     dset_PATHS = h5_file.create_dataset("root_files",
-                                  shape=(num_events,),
+                                  shape=(num_nonzero_events,),
                                   dtype=dtype_PATHS)
     dset_angles = h5_file.create_dataset("angles",
-                                 shape=(num_events, 2),
+                                 shape=(num_nonzero_events, 2),
                                  dtype=dtype_angles)
     
     # 22 -> gamma, 11 -> electron, 13 -> muon
@@ -102,80 +107,62 @@ if __name__ == '__main__':
     #pid_to_label = {22:0, 11:1, 13:2}
     
     offset = 0
-    
+    offset_next = 0
     mPMT_to_index = GenMapping(PMT_LABELS)
-    for filename in files:
-        data = np.load(filename)
-        num_events_in_file = len(data['digi_hit_pmt'])
-        x_data = np.zeros((num_events_in_file,)+IMAGE_SHAPE, 
+    for file_index, filename in enumerate(files):
+        data = np.load(filename, allow_pickle=True)
+        nonzero_events_in_file = len(nonzero_event_indexes[file_index])
+        x_data = np.zeros((nonzero_events_in_file,)+IMAGE_SHAPE, 
                           dtype=dtype_events)
         digi_hit_pmt = data['digi_hit_pmt']
         digi_hit_charge = data['digi_hit_charge']
         digi_hit_time = data['digi_hit_time']
-        for i in range(num_events_in_file):
+        delay = 0
+        for i in range(len(digi_hit_pmt)):
             hit_pmts = digi_hit_pmt[i]
+            if len(hit_pmts) == 0:
+                delay += 1
+                continue
             charge = digi_hit_charge[i]
             time = digi_hit_time[i]
             for j in range(len(hit_pmts)):
                 hit_mpmt = hit_pmts[j] // 19
                 pmt_channel = hit_pmts[j] % 19
                 index = mPMT_to_index[hit_mpmt]
-                x_data[i, index[0], index[1], pmt_channel] = charge[j]
-                x_data[i, index[0], index[1], pmt_channel + 19] = time[j]
-        directions = data['direction']
-        if 'IWCDmPMT_4pi_full_tank_gamma' in filename:
-            labels = np.zeros(num_events_in_file)
-            e_mass=0.51099895000
-            energies=np.expand_dims(data['energy'],1)
-            mag_momenta=np.sqrt(energies**2-e_mass**2)
-            mag_momenta=np.expand_dims(mag_momenta,2)
-            momenta=mag_momenta*directions
-            momenta_sum=np.sum(momenta,axis=1)
-            momenta_sum_adj=momenta_sum[:,[2,0,1]]
-            momenta_sum_adj_mag=np.sqrt(np.sum(momenta_sum_adj**2,axis=1))
-            directions_proper=momenta_sum_adj/np.expand_dims(momenta_sum_adj_mag,1)
-            energies = np.sum(energies, axis=1).reshape(-1,1)
-            positions = np.expand_dims(data['position'],1)[:,0,:].reshape(-1, 1,3)
-        elif 'IWCDmPMT_4pi_full_tank_e-' in filename:
-            labels = np.ones(num_events_in_file)
-            energies = np.expand_dims(data['energy'],1)
-            positions = np.expand_dims(data['position'],1)
-            directions = directions.squeeze()
-            directions_proper = directions[:,[2,0,1]]
-        elif 'IWCDmPMT_4pi_full_tank_mu-' in filename:
-            labels = np.full(num_events_in_file, 2)
-            energies = np.expand_dims(data['energy'],1)
-            positions = np.expand_dims(data['position'],1)
-            directions = directions.squeeze()
-            directions_proper = directions[:,[2,0,1]]
-        else:
-            raise ValueError("File {} is not electron, muon or gamma".format(filename))
-        energies = energies.astype(dtype_energies)
-        positions = positions.astype(dtype_positions)
+                x_data[i - delay, index[0], index[1], pmt_channel] = charge[j]
+                x_data[i - delay, index[0], index[1], pmt_channel + 19] = time[j]
         
-        planar_mag=np.sqrt(np.sum(directions_proper[:,[0,1]]**2,axis=1))
-        azimuthal=np.arctan2(directions_proper[:,1],directions_proper[:,0])
-        polar=np.arctan2(directions_proper[:,2],planar_mag)
-        azimuthal=azimuthal.reshape(-1,1)
-        polar=polar.reshape(-1,1)
-        angles=np.hstack((polar,azimuthal))
+        event_id = data['event_id']
+        root_file = data['root_file']
+        pid = data['pid']
+        position = data['position']
+        direction = data['direction']
+        energy = data['energy'] 
         
-        PATHS = data['root_file'].astype(dtype_PATHS)
-        IDX = data['event_id'].astype(dtype_IDX)
+        offset_next += nonzero_events_in_file 
         
-        offset_next = offset + num_events_in_file
+        file_indices = nonzero_event_indexes[file_index]
 
+        dset_IDX[offset:offset_next] = event_id[file_indices]
+        dset_PATHS[offset:offset_next] = root_file[file_indices]
+        dset_energies[offset:offset_next,:] = energy[file_indices].reshape(-1,1)
+        dset_positions[offset:offset_next,:,:] = position[file_indices].reshape(-1,1,3)
+        
+        labels = np.full(pid.shape[0], -1)
+        labels[pid==22] = 0
+        labels[pid==11] = 1
+        labels[pid==13] = 2
+        dset_labels[offset:offset_next] = labels[file_indices]
+
+        direction = direction[file_indices]
+        polar = np.arccos(direction[:,1])
+        azimuth = np.arctan2(direction[:,2], direction[:,0])
+        dset_angles[offset:offset_next,:] = np.hstack((polar.reshape(-1,1),azimuth.reshape(-1,1)))
         dset_event_data[offset:offset_next,:] = x_data
-        dset_labels[offset:offset_next] = labels
-        dset_energies[offset:offset_next,:] = energies
-        dset_positions[offset:offset_next,:,:] = positions
-        dset_PATHS[offset:offset_next] = PATHS
-        dset_IDX[offset:offset_next] = IDX
-        dset_angles[offset:offset_next,:] = angles
         
         offset = offset_next
         print("Finished file: {}".format(filename))
         
     print("Saving")
-    f.close()
+    h5_file.close()
     print("Finished")
